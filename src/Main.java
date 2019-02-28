23a3aa4e35aafa0cfdcdd6181df89a1f5aa08340
Filip Tepes-Onea <filip.tepes@outlook.com>
2019-02-28 23:16:08 +0200

    good

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,7 @@ import utils.Writer;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -40,12 +37,14 @@ public class Main {
             }
         });
 
+
+
+
         try {
-            Writer.writeToFile(new PrintStream(new File("out_" + args[0])), horizontalPhotos);
+            Writer.writeToFile(new PrintStream(new File("out_" + args[0])), solutionOne(horizontalPhotos));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-
 
 
     }
@@ -58,17 +57,55 @@ public class Main {
         return rv;
     }
 
+
     public static ArrayList<Photo> solutionOne(ArrayList<Photo> photos){
-        ArrayList<Photo> rv = new ArrayList<>();
-        for (int i = 0; i < photos.size() - 1; i++) {
-            for (int j = i; j < photos.size(); j++) {
+        //ArrayList<Photo> rv = new ArrayList<>();
+        for (int i = 0; i < photos.size() - 3; i++) {
+            //for (int j = i+1; j < photos.size() - 1; j++) {
                 float commonTags = photos.get(i)
-                        .getCommonNoOfTags(photos.get(j));
-                float minNumberOfTags = photos.get(i)
-                        .getMinNumberOfTags(photos.get(j));
+                        .getCommonNoOfTags(photos.get(i+1));
+//                float minNumberOfTags = photos.get(i)
+//                        .getMinNumberOfTags(photos.get(i+1));
+
+            float commonTags2 = photos.get(i)
+                    .getCommonNoOfTags(photos.get(i+2));
+//            float minNumberOfTags2 = photos.get(i)
+//                    .getMinNumberOfTags(photos.get(i+2));
+
+                if (commonTags2 > commonTags) {
+
+                    Photo aux = photos.get(i+1);
+                    photos.set(i+1, photos.get(i+2));
+                    photos.set(i+2, aux);
+
+
+                }
+
+
+//                if (0 <= commonTags && commonTags < 3/4 * minNumberOfTags) {
+//                    Photo aux = photos.get(i+1);
+//                    photos.set(i+1, photos.get(j));
+//                    photos.set(j, aux);
+//                    break;
+//                }
 
             }
-        }
-        return rv;
+
+        return photos;
     }
+//
+//    public static ArrayList<Photo> solutionHash(ArrayList<Photo> photos){
+//        ArrayList<Photo> rv = new ArrayList<>();
+//        HashMap<String, List<Photo>> photosHashMap = new HashMap<>();
+//        for(Photo photo: photos){
+//            for(String tag:  photo.getTags())
+//        }
+//        for(String tag: photos.keySet()){
+//            rv.add(photos.get(tag));
+//        }
+//
+//        return rv;
+//    }
+
+
 }
